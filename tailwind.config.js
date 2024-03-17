854edebb5e5ee36192996291df1c96e82b4e7ca5
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-03-17 13:06:27 -0400

    Style index, item detail, item create/update pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./views/**/*.ejs'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: '"Lora", Georgia, Cambria, "Times New Roman", Times, serif',
+        sans: '"Roboto", Helvetica, sans-serif'
+      },
+    },
+    screens: {
+      'sm': '475px', // default: 640px
+      'md': '768px', // default: 768px
+      'lg': '900px', // default: 1024px
+      'xl': '1280px', // default: 1280px
+      '2xl': '1536px', // default: 1536px
+    }
   },
   plugins: [],
 };
